3e676a518f7c47dd347e49f73d853c73c508e0a7
Cesar Gutierrez Espitia <ieethczar@gmail.com>
2017-08-14 20:10:58 -0500

    Hata aqui funciona el servicio de login pero hay que cambiar el puerto en el archivo www

--- routes/bienvenido.js
+++ routes/bienvenido.js
@@ -2,17 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/bienvenido', function(req, res, next) {
-    var db=req.db;
-    var usuarios=db.get('usuarios');
-    usuarios.find({usuario:req.user,password:req.password},'-_id',function(e,docs){        
-        if(docs.length>0){
-            res.render('bienvenido')
+router.post('/', function (req, res, next) {
+    var db = req.db;
+    var usuarios = db.get('usuarios');
+    usuarios.count({
+        usuario: req.body.user,
+        password: req.body.password
+    }).then(function (cuenta) {
+        if (cuenta > 0) {
+            res.render('bienvenido');
         }else{
-            res.render('index')
+            res.render('index',{
+                mensaje:"Error!: Usuario no encontrado"
+            });
         }
-    }),function(){
-        console.log(e)
-    }
-})
+        db.close();
+    });
+}); 
 module.exports = router;
